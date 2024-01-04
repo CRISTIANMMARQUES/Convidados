1897f84d165b2793c6e5fb04d82f60a37fd5d7d1
cristianmmarques <cristianmedina616@gmail.com>
2024-01-04 16:17:11 -0400

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/convidados/viewModel/GuestFormViewModel.kt
+++ app/src/main/java/com/example/convidados/viewModel/GuestFormViewModel.kt
@@ -2,15 +2,31 @@ package com.example.convidados.viewModel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 
 import com.example.convidados.model.GuestModel
 import com.example.convidados.repository.GuestRepository
 
-class GuestFormViewModel(application: Application): AndroidViewModel(application) {
+class GuestFormViewModel(application: Application) : AndroidViewModel(application) {
 
     private val repository = GuestRepository.getInstance(application)
 
-    fun insert(guest: GuestModel){
-        repository.insert(guest)
+    private val guestModel = MutableLiveData<GuestModel>()
+    val guest: LiveData<GuestModel> = guestModel
+
+    private val _saveGuest = MutableLiveData<Boolean>()
+    val saveGuest: LiveData<Boolean> = _saveGuest
+
+    fun save(guest: GuestModel) {
+        if (guest.id == 0) {
+            _saveGuest.value = repository.insert(guest)
+        } else {
+            _saveGuest.value = repository.update(guest)
+        }
+    }
+
+    fun get(id: Int) {
+        guestModel.value = repository.get(id)
     }
 }
