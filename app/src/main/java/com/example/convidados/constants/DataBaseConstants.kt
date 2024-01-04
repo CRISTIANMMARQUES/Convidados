98c38cf191ba471371d58e43708f01557f05fc35
cristianmmarques <cristianmedina616@gmail.com>
2024-01-04 16:31:25 -0400

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/convidados/constants/DataBaseConstants.kt
+++ app/src/main/java/com/example/convidados/constants/DataBaseConstants.kt
@@ -1,10 +1,11 @@
 package com.example.convidados.constants
 
 class DataBaseConstants private constructor(){
-    object GUEST{
+    object GUEST {
+        const val ID = "guestid"
         const val TABLE_NAME = "Guest"
 
-        object COLUMNS{
+        object COLUMNS {
             const val ID = "id"
             const val NAME = "name"
             const val PRESENCE = "presence"
--- app/src/main/java/com/example/convidados/view/GuestFormActivity.kt
+++ app/src/main/java/com/example/convidados/view/GuestFormActivity.kt
@@ -1,10 +1,14 @@
+
 package com.example.convidados.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.convidados.R
+import com.example.convidados.constants.DataBaseConstants
 import com.example.convidados.databinding.ActivityGuestFormBinding
 import com.example.convidados.model.GuestModel
 import com.example.convidados.viewModel.GuestFormViewModel
@@ -12,8 +16,11 @@ import com.example.convidados.viewModel.GuestFormViewModel
 class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var viewModel: GuestFormViewModel
+
     private var guestId: Int = 0
+
     private lateinit var binding: ActivityGuestFormBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,17 +30,53 @@ class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
 
         viewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
 
-        binding.buttonSave.setOnClickListener(this)
-        binding.radioPresent.isChecked = true
+        observe()
+
+        loadData()//carregando os dados do BD
     }
 
     override fun onClick(v: View) {
-        if(v.id == R.id.button_save){
-            val name = binding.editName.text.toString()//convertendo o texto em string
+
+        if (v.id == R.id.button_save) {
+            val name = binding.editName.text.toString()
             val presence = binding.radioPresent.isChecked
 
-            val model = GuestModel(0,name, presence)
-            viewModel.insert(model)
+            val model = GuestModel(guestId, name, presence)
+            viewModel.save(model)
+        }
+    }
+
+    private fun observe(){
+
+        viewModel.guest.observe(this, Observer {
+            binding.editName.setText(it.name)
+            if(it.presence){
+                binding.radioPresent.isChecked = true
+            }else{
+                binding.radioAbsent.isChecked = true
+            }
+        })
+
+        viewModel.saveGuest.observe(this, Observer {
+            if(it){
+                if(guestId == 0){
+                    Toast.makeText(applicationContext,"Inserção com sucesso",Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(applicationContext,"Atualização com sucesso",Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(applicationContext,"Falha",Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun loadData() {
+        val bundle = intent.extras
+        if (bundle != null) {
+            guestId = bundle.getInt(DataBaseConstants.GUEST.ID)
+            viewModel.get(guestId)
         }
     }
 }
+
+
