d4a8e70a4d2a599d7393d56948fe021027f5958e
cristianmmarques <cristianmedina616@gmail.com>
2024-03-04 16:07:46 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/convidados/repository/GuestRepository.kt
    #	app/src/main/java/com/example/convidados/view/GuestFormActivity.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -49,6 +50,8 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
     implementation("androidx.media3:media3-common:1.2.0")
+    implementation("androidx.room:room-runtime:2.4.3")
+    kapt ("androidx.room:room-compiler:2.4.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/convidados/repository/GuestDataBaseHelper.kt
+++ app/src/main/java/com/example/convidados/repository/GuestDataBaseHelper.kt
@@ -6,31 +6,50 @@ package com.example.convidados.repository
 import android.database.sqlite.SQLiteOpenHelper
 import android.database.sqlite.SQLiteDatabase
 import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.convidados.constants.DataBaseConstants
+import com.example.convidados.model.GuestModel
 
-class GuestDataBaseHelper(context: Context): SQLiteOpenHelper(context, NAME, null, VERSION){
+@Database(entities = [GuestModel::class], version = 1)
+abstract class GuestDataBaseHelper: RoomDatabase(){
+    companion object{
+        private lateinit var INSTANCE: GuestDataBaseHelper
+        fun getDataBase(context: Context): GuestDataBaseHelper{
+            if(!::INSTANCE.isInitialized){
+                synchronized(GuestDataBaseHelper::class) {
+                    INSTANCE =
+                        Room.databaseBuilder(context, GuestDataBaseHelper::class.java, "guestdb")
+                            .addMigrations()
+                            .allowMainThreadQueries()
+                            .build()
+                }
+            }
+            return INSTANCE
+        }
+        private val MIGRATION_1_2: Migration = object : Migration(1,2){
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("DELETE FROM Guest")
+            }
+
+        }
+    }
+
+//class GuestDataBaseHelper(context: Context): SQLiteOpenHelper(context, NAME, null, VERSION){
 
     /**
      * Método executado somente uma vez quando o acesso ao banco de dados é feito pela primeira vez
      */
-    override fun onCreate(db: SQLiteDatabase) {
+    /*override fun onCreate(db: SQLiteDatabase) {
         db.execSQL(CREATE_TABLE_GUEST)
-    }
+    }*/
 
     /**
      * Método executado quando a versão do DATABASE_VERSION é alterada
      * Dessa maneira, a aplicação sabe que o banco de dados foi alterado e é necessário rodar o script de update
      */
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
 
-    companion object{
-        private const val VERSION = 1
-        private const val NAME = "Convidados.db"
-
-        private const val CREATE_TABLE_GUEST =
-            ("create table " + DataBaseConstants.GUEST.TABLE_NAME + " ("
-                    + DataBaseConstants.GUEST.COLUMNS.ID + " integer primary key autoincrement, "
-                    + DataBaseConstants.GUEST.COLUMNS.NAME + " text, "
-                    + DataBaseConstants.GUEST.COLUMNS.PRESENCE + " integer);")
-    }
 }
